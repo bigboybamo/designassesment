6bf1f813b20b692c8911c6fb6e1d6b01f6444a89
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-12-05 12:07:52 +0100

    completed the form part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from "react-bootstrap"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App">
+    <div  className="main-container">
+  <Row>
+    <Col><h2 className="talk-header">Lets Talk</h2>
+    <p>To request a quote or want to meet up for coffee</p>
+    <p>contact us directly or fill out the form and we will</p>
+    <p>get back to you promptly</p>
+    <form>
+    <label for="male">Your Name:</label><br></br>
+    <input type="text" className="input-field"/><br></br>
+    <label for="male">Your Email:</label><br></br>
+    <input type="text" className="input-field"/><br></br>
+    <label for="male">Your Message:</label><br></br>
+    <textarea rows="4" cols="50" className="input-field1">Type something you want to send...</textarea>
+    <button className ="form-btn">Send Message</button>
+    </form>
+    
+    </Col>
+    <Col>2 of 2</Col>
+  </Row>
+  </div>
+
+  </div>
 }
 
-export default App;
+export default App
